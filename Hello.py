88398bad68432506d1fecfd1a64e7f5e9a157c8e
SouLKP <142486241+SouLKP@users.noreply.github.com>
2023-10-19 12:41:16 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -19,10 +19,7 @@ LOGGER = get_logger(__name__)
 
 
 def run():
-    # st.set_page_config(
-    #     page_title="Hello",
-    #     page_icon="👋",
-    # )
+
 
     st.write("# Welcome to KP's Streamlit! 👋 ")
 
